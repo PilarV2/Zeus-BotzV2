0936e09f6cae8ec343a89851ad97840a07f81549
PilarV2 <97302454+PilarV2@users.noreply.github.com>
2022-02-07 14:36:52 +0700

    Add files via upload

--- plugins/darkjoke.js
+++ plugins/darkjoke.js
@@ -1,11 +1,8 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let url = global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY')
-    await conn.sendButtonImg(m.chat, url,  footer, 'Next', `${usedPrefix + command}`, m, 0, { thumbnail: await (await fetch(url)).buffer() })
-    // await conn.sendFile(m.chat, url, '', '', m, 0, { thumbnail: await (await fetch(url)).buffer() })
+let handler = async(m, { conn }) => {
+conn.sendFile(m.chat, global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'), '', 'Gelap Woy Siapa Yang Matiin Lampu?',m)
 }
-handler.help = ['darkjoke']
-handler.tags = ['internet']
-handler.command = /^((drag|dark)joke|jokes)$/i
+handler.help = ['darkjokes']
+handler.tags = ['image']
+handler.command = /^(dragjoles|darkjokes)$/i
 
 module.exports = handler
--- plugins/meme_indo.js
+++ plugins/meme_indo.js
@@ -2,12 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=caliph_71')
   let barbar = await url.json()
-conn.sendButtonImg(m.chat, await (await fetch(barbar.result)).buffer(), '*MEME INDO*', watermark, 'NEXT', '.memeindo', m)
+conn.sendFile(m.chat, barbar.result, 'memeindo', JSON.stringify(barbar, null, 2), m)
 }
-handler.help = ['memeindo']
-handler.tags = ['fun']
-handler.command = /^(memeindo|memein)$/i
+handler.command = /^(memeindo)$/i
+handler.register = true
 handler.private = false
-handler.limit = false
+handler.limit = true
 
 module.exports = handler
