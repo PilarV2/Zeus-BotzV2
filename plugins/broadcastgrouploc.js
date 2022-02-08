5d4630db14e238ba2026d44ee70ddcca7308d06f
PilarTomlol <97302454+PilarV2@users.noreply.github.com>
2022-02-08 08:52:19 +0700

    Add files via upload

---
+++ plugins/broadcastgrouploc.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+	let logo = global.image
+    let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
+    conn.reply(m.chat, `_Send a broadcast message to ${groups.length} group_\nestimation complete ${groups.length * 1.5} seconds`, m)
+    for (let id of groups) {
+        await delay(1500)
+        await conn.send2ButtonLoc(id, logo,'*—「 Broadcast 」—*\n' + text, ZeusBotz, '⋮☰ Menu', '.menu', 'Sewa', '.sewa')
+    }
+    m.reply('_*Broadcast Finished*_')
+}
+handler.help = ['bcgcloc'].map(v => v + ' <teks>')
+handler.tags = ['owner']
+handler.command = /^(broadcast|bc)(gcloc|groupsloc)$/i
+
+handler.owner = true
+
+module.exports = handler
+
+const delay = time => new Promise(res => setTimeout(res, time))
+
