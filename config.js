63c924034dbb46b6fa1e899401cc23d104171322
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-02-10 17:56:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ watermark = 'ZeusBotz' //ganti aja
 
 // 
 wait = '_*Tunggu Sebentar...*_'
-global.botwm = 'ZeusBotz 😱'
+global.botwm = 'ZeusBotz'
 global.wait = '_*Tunggu Sebentar...*_'
 global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9'
 global.nsfw = 'Fitur NSFW Dimatikan\nKetik *!enable* *nsfw* untuk menggunakan fitur ini!\n“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mereka menahan pandanganya, dan memelihara kemaluannya; … Katakanlah kepada wanita yang beriman: Hendaklah mereka menahan pandangannya, dan kemaluannya, dan janganlah mereka Menampakkan perhiasannya, kecuali yang (biasa) nampak dari padany,” \n(TQS. Al-Nur [24]: 30-31).'
