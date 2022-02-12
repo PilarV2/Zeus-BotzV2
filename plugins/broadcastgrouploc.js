2c6496546c06a0f482f38795fd27777846bdf256
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-02-12 08:58:04 +0700

    Update broadcastgrouploc.js

--- plugins/broadcastgrouploc.js
+++ plugins/broadcastgrouploc.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn, text }) => {
     let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
     conn.reply(m.chat, `_Send a broadcast message to ${groups.length} group_\nestimation complete ${groups.length * 1.5} seconds`, m)
     for (let id of groups) {
-        await delay(1500)
-        await conn.sendButtonLoc(id, await (await fetch('http://xosfor.herokuapp.com/api/matrix?q=Broadcast&apikey=8uhDyCUd')).buffer(), text, wm, 'Broadcast', 'broadcast')
+        await delay(1300)
+        await conn.sendButtonLoc(id, await (await fetch('http://xosfor.herokuapp.com/api/matrix?q=Broadcast&apikey=8uhDyCUd')).buffer(), text, wm, 'MENU', '.menu')
     }
     m.reply('_*Broadcast Finished*_')
 }
-handler.help = ['bcloc'].map(v => v + ' <teks>')
+handler.help = ['bcgcloc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(broadcast|bc)(loc)$/i
+handler.command = /^(broadcastgc|bcgc)(loc)$/i
 
 handler.owner = true
 
