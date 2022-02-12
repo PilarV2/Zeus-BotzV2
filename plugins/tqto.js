126c8a0c9a88bd2d15099b1f55e262faab31c51c
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-02-12 20:51:27 +0700

    Update tqto.js

--- plugins/tqto.js
+++ plugins/tqto.js
@@ -1,5 +1,5 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(thanks)).buffer(), `
+let handler = async (m, { conn }) =>)).buffer()conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 BIG THANKS TO
 
 •Allah swt
@@ -37,3 +37,43 @@ module.exports = handler
 
 // di ilangin jangan,di tambahin boleh
 
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Thanks To')).buffer(), `
+BIG THANKS TO
+
+•Allah swt
+•My ortu
+•Nurutomo
+•Adiwajshing
+•Ariffb
+•Amel
+•IrwanX
+•Dawnfrosty
+•Rteam1
+•Beniismael
+•Raditya 
+•AlyaaXzy ( Sc Ori )
+•Furqan
+•Elyas
+•Rasel
+•Xteam
+•khael
+•Atenabot
+•Baka Botz
+•ZeeoneOfc
+•Zeks
+•Rendycraft  
+•Nadin
+•Aca Mirabel
+•Zeus Botz ( gwhej )
+•The.SadBoy.01
+•Penyedia Layanan API
+•Orang-orang yang Berdonasi
+`.trim(), watermark, 'Back', '.menu')
+handler.help = ['Thanksto', 'tqto']
+handler.tags = ['main']
+handler.command = /^(tqto|thanks|thanksto|bigthanks)$/i
+
+module.exports = handler
+
+// di ilangin jangan,di tambahin boleh
