01107520fc5be0ca866d711d93f13c2965cd6ade
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-02-12 20:49:02 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 *────────「 SCRIPT 」 ────────*
-Hai 👋
-saya AlyaaXzy_1
-Bot Ini Menggunakan Sc dari github
--
-`.trim(), '© AlyaaXzy', 'Thanks', '.tqto')
+> Script Ori = https://github.com/AlyaaXd/rfkbot
+
+> Script Recode By Pilar = https:/github.com/PilarV2/Zeus-BotzV2
+
+`.trim(), '© ZeusBotz', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
