466da28686ddc0c51190c5156f66d78303aac8f3
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-02-26 08:48:42 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -3,16 +3,14 @@ let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╭═══ *〘 SEWA 〙*
 ║ ┅ ๑————————————๑
 ║┊ ⌲ Sewa = _5k/grup (1 minggu)_
-║┊ ⌲ Sewa = _15k/grup (1 bulan)_
-║┊ ⌲ Sewa = _20k/grup (2 bulan)_
-║┊ ⌲ Sewa = _35k/grup (8 bulan)_                                                                      ║┊ ⌲ Sewa = _45k/grup (12 bulan)_
+║┊ ⌲ Sewa = _10k/grup (1 bulan)_
+║┊ ⌲ Sewa = _20k/grup (Permanen)_                                                                    ║┊ ⌲ Sewa = _45k/grup (12 bulan)_
 ╰═ ┅ ═══════
 ╭═══ *〘 BELI PREMIUM 〙*
 ║ ┅ ๑————————————๑
-║┊ ⌲ Premium = _10k/minggu_
-║┊ ⌲ Premium = _35k/bulan_
-║┊ ⌲ Premium = _46k/permanen
-║┊ ⌲ Jadi Owner = _75k/permanen
+║┊ ⌲ Premium = _5k/minggu_
+║┊ ⌲ Premium = _20k/bulan_
+║┊ ⌲ Premium = _35k/permanen
 ╰═ ┅ ═══════
 
 ༅ _*KEUNTUNGAN SEWABOT*_
